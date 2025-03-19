17bfa3df5fe75efceb68a188c08af7dae6f5caf2
bharathkapril <bharathapril25@gmail.com>
2025-03-19 17:39:43 +0530

    Finished code

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,8 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const LoadingDashboard = () => {
+  return <Skeleton className="w-full h-[400px]" />;
+};
+
+export default LoadingDashboard;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "m.media-amazon.com",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "avatars.githubusercontent.com",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
